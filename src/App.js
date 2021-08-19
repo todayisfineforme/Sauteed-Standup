e93135f68cd5e9eb02ac3b948c003bc32b678a8b
Nathan Hall <Nashing1@gmail.com>
2021-08-18 21:38:18 -0500

    added bootstrap logos
    added bootstrap and react bootstrap and added svg logos and new favicon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
+import sauteeLogo from './images/Full Logo.svg';
+// import {ReactComponent as sauteeLogo} from './images/SimplifiedLogo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={sauteeLogo} className="App-logo" alt="logo" />
+        {/* <SauteeLogo/> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <a 
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
