4eb3d5078db66f14282ad90600831b66717e653d
Nathan Hall <Nashing1@gmail.com>
2021-10-04 10:23:16 -0500

    Next show date display
    added function to tickets component to ensure next show date is always properly displayed

--- src/components/tickets.js
+++ src/components/tickets.js
@@ -1,15 +1,37 @@
 import '../style/tickets.css';
 import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import upcomingDates from '../info/upcomingDates';
 
 
 function Tickets(props){
+    
+
+    let nextShow = "";
+    const current = new Date();
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const days = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "31st"];
+    
+    for (var i= 0; i<upcomingDates.length; i++){
+
+        let nextYear = upcomingDates[i].year;
+        let nextMonth = upcomingDates[i].month;
+        let nextDay = upcomingDates[i].day;
+
+        const next = new Date(nextYear, nextMonth, nextDay, 21);
+
+        if (current < next){
+            nextShow = months[nextMonth] + " " + days[nextDay];
+            break;
+        };
+    };
+
     return(
         <Container className={props.containerSpace} fluid>
             <Row className={props.rowSpace}>
                 <h1>Next Show is:</h1>
             </Row>
             <Row className={props.rowSpace}>
-                <Button variant="outline-light" size="lg" href="https://www.simpletix.com/e/sauteed-standup-tickets-77571" target="_blank"><h1>Sunday <span>September 5th</span></h1></Button>
+                <Button variant="outline-light" size="lg" href="https://www.simpletix.com/e/sauteed-standup-tickets-77571" target="_blank"><h1>Sunday <span>{nextShow}</span></h1></Button>
             </Row>
         </Container>
     );
---
+++ src/info/upcomingDates.js
@@ -0,0 +1,29 @@
+const upcomingDates = [
+    {
+        year: 2021,
+        month : 9,
+        day: 16
+    },
+    {
+        year: 2021,
+        month : 10,
+        day: 6
+    },
+    {
+        year: 2021,
+        month : 10,
+        day: 20
+    },
+    {
+        year: 2021,
+        month : 11,
+        day: 4
+    },
+    {
+        year: 2021,
+        month : 11,
+        day: 18
+    },
+];
+
+export default upcomingDates;
