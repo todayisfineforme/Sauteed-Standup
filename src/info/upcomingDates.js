e9de69b6ee29c032bd23fb6837cab0d9ef073fb4
Nathan Hall <Nashing1@gmail.com>
2022-05-17 15:05:15 -0500

    updated show dates

--- src/info/upcomingDates.js
+++ src/info/upcomingDates.js
@@ -1,29 +1,69 @@
 const upcomingDates = [
     {
-        year: 2021,
-        month : 9,
-        day: 16
+        year: 2022,
+        month : 2,
+        day: 19
     },
     {
-        year: 2021,
-        month : 10,
-        day: 6
+        year: 2022,
+        month : 3,
+        day: 2
     },
     {
-        year: 2021,
-        month : 10,
-        day: 20
+        year: 2022,
+        month : 4,
+        day: 0
     },
     {
-        year: 2021,
-        month : 11,
+        year: 2022,
+        month : 4,
+        day: 14
+    },
+    {
+        year: 2022,
+        month : 5,
         day: 4
     },
     {
-        year: 2021,
-        month : 11,
+        year: 2022,
+        month : 5,
         day: 18
     },
+    {
+        year: 2022,
+        month : 6,
+        day: 16
+    },
+    {
+        year: 2022,
+        month : 7,
+        day: 6
+    },
+    {
+        year: 2022,
+        month : 7,
+        day: 20
+    },
+    {
+        year: 2022,
+        month : 8,
+        day: 3
+    },
+    {
+        year: 2022,
+        month : 8,
+        day: 17
+    },
+    {
+        year: 2022,
+        month : 9,
+        day: 1
+    },
+    {
+        year: 2022,
+        month : 9,
+        day: 15
+    }
 ];
 
 export default upcomingDates;
