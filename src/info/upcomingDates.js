825781bd0bdd89366c772338a55441a24cda2bb3
Nathan Hall <Nashing1@gmail.com>
2022-11-28 20:02:54 -0600

    updated dates
    updated dates for first half of 2023

--- src/info/upcomingDates.js
+++ src/info/upcomingDates.js
@@ -38,6 +38,76 @@ const upcomingDates = [
         year: 2022,
         month : 11,
         day: 18
+    },
+    {
+        year: 2023,
+        month : 0,
+        day: 8
+    },
+    {
+        year: 2023,
+        month : 0,
+        day: 15
+    },
+    {
+        year: 2023,
+        month : 0,
+        day: 22
+    },
+    {
+        year: 2023,
+        month : 0,
+        day: 29
+    },
+    {
+        year: 2023,
+        month : 1,
+        day: 5
+    },
+    {
+        year: 2023,
+        month : 1,
+        day: 19
+    },
+    {
+        year: 2023,
+        month : 2,
+        day: 5
+    },
+    {
+        year: 2023,
+        month : 2,
+        day: 19
+    },
+    {
+        year: 2023,
+        month : 3,
+        day: 2
+    },
+    {
+        year: 2023,
+        month : 3,
+        day: 16
+    },
+    {
+        year: 2023,
+        month : 4,
+        day: 7
+    },
+    {
+        year: 2023,
+        month : 4,
+        day: 21
+    },
+    {
+        year: 2023,
+        month : 5,
+        day: 4
+    },
+    {
+        year: 2023,
+        month : 5,
+        day: 18
     }
 ];
 
